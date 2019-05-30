8befb9cad1f9bb4cd64f7d1067b0ebea3e3c0820
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-05-30 13:00:55 -0400

    Set up a basic UI
    Added general UI stuff, buttons needed + collection view with proper constraints

---
+++ MineSweeper/MineSweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Tristan Pudell-Spatscheck on 5/30/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
